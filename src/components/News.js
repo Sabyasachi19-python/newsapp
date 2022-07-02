f31df4a778160922d068fddb17f64fe164ab9e48
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-07-02 10:21:16 +0530

    video28

--- src/components/News.js
+++ src/components/News.js
@@ -2,37 +2,67 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-
   constructor() {
     super();
     console.log('Hi, this is constructor!');
     this.state = {
       articles: [],
-      loading: false
+      loading: false,
+      page: 1
     }
   }
-  async componentDidMount(){
-    console.log("CDM");
-    let url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=25f116dbfc264c329d6bf8c5cceea572';
+
+  handlePreviousClick = async () => {
+    console.log('previous');
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=25f116dbfc264c329d6bf8c5cceea572&page=${this.state.page - 1}&pageSize=20`;
     let data = await fetch(url);
     let parsed_data = await data.json();
     console.log(parsed_data);
-    this.setState({articles:parsed_data.articles});
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsed_data.articles
+    });
+  }
+
+  handleNextClick = async () => {
+    console.log('next');
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+    }
+    else {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=25f116dbfc264c329d6bf8c5cceea572&page=${this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsed_data = await data.json();
+      console.log(parsed_data);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsed_data.articles
+      });
+    }
+  }
+  async componentDidMount() {
+    console.log("CDM");
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=25f116dbfc264c329d6bf8c5cceea572&page=${this.state.page}&pageSize=20`;
+    let data = await fetch(url);
+    let parsed_data = await data.json();
+    console.log(parsed_data);
+    this.setState({ articles: parsed_data.articles, totalResults: parsed_data.totalResults });
   }
   render() {
     console.log("Render");
     return (
       <div className="container my-3">
         <h1>News SR Top Headlines</h1>
-
         <div className="row">
           {this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url}>
-              <NewsItem imageUrl={element.urlToImage} title={element.title?element.title:""} description={element.description?element.description:""} newsURL={element.url} />
+              <NewsItem imageUrl={element.urlToImage} title={element.title ? element.title : ""} description={element.description ? element.description : ""} newsURL={element.url} />
             </div>
           })}
-
-
+          <div className="container d-flex justify-content-between">
+            <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>&larr; Previous Page</button>
+            <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next Page &rarr;</button>
+          </div>
         </div>
       </div>
     )
