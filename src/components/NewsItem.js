3d699bb9a46eac9dcb269ab50aa9889c3e298ffc
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-07-04 09:42:17 +0530

    #32 adding time, author and news source

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,14 +3,16 @@ import newslogo from './news.webp'
 //api-key: 25f116dbfc264c329d6bf8c5cceea572
 export class NewsItem extends Component {
     render() {
-        let { title, description, imageUrl, newsURL } = this.props;
+        let { title, description, imageUrl, newsURL, author, date, source} = this.props;
         return (
             <div className="my-3">
                 <div className="card"> 
+                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-success" style={{left: '90%', zIndex:1}}>{source}</span>
                     <img src={imageUrl?imageUrl:newslogo} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
+                        <h5 className="card-title">{title} <span className="badge bg-secondary">New</span> </h5>
                         <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {author?author:"unknown"} on {new Date(date).toGMTString()}</small></p>
                         <a href={newsURL} target="_blank" className="btn btn-sm btn-primary" rel="noreferrer">Read More</a>
                     </div>
                 </div>
