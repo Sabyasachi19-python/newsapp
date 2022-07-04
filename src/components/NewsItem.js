5e2cfaaf4d17852c76655edb10f634eea9e45268
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-07-04 09:45:28 +0530

    added minor change (removed new badge)

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,7 +10,7 @@ export class NewsItem extends Component {
                 <span className="position-absolute top-0 translate-middle badge rounded-pill bg-success" style={{left: '90%', zIndex:1}}>{source}</span>
                     <img src={imageUrl?imageUrl:newslogo} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title} <span className="badge bg-secondary">New</span> </h5>
+                        <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
                         <p className="card-text"><small className="text-muted">By {author?author:"unknown"} on {new Date(date).toGMTString()}</small></p>
                         <a href={newsURL} target="_blank" className="btn btn-sm btn-primary" rel="noreferrer">Read More</a>
